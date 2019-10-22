8862dacf98c41582f569a811977e1b03a597cf41
mauriciosoares01 <mss.soares@hotmail.com>
2019-10-22 02:44:50 -0300

    adjust interests to quarter

--- dataService.py
+++ dataService.py
@@ -1,12 +1,22 @@
+# coding=utf-8
+
 import pandas as pd
 
-def header(msg):
-    print("-" * 50)
-    print("[" + msg + "]")
+# def header(msg):
+#     print("-" * 50)
+#     print("[" + msg + "]")
     
-def read():
-    return pd.read_csv("financialData.csv")
+def financial():
+    financialData = pd.read_csv("financialData.csv")
+    return(financialData)
+
+def ipca():
+    ipca = pd.read_csv("ipca.csv", index_col ="Mês/Ano")
+    return(ipca)
+
+def selic():
+    selic = pd.read_csv("selic.csv", index_col ="Mês/Ano")
+    return(selic)
 
 
-header("Resultados financeiros")
-print(read())
+
--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
 import pandas as pd
+import dataService
 
-def Main():
-    yearsVector = []    #stores the sequence of years in the financial demonstrative
-    mainTable = []      #stores the values of the financial demonstrative by quarter
+def main():
+    financialData = dataService.financial()      #stores the values of the financial demonstrative by quarter
+    ipca = dataService.ipca()
+    selic =dataService.selic()   
+
+    adusted_ipca = adjust_values_to_quarter(ipca)
+    adusted_selic = adjust_values_to_quarter(selic)
+
+    print(adusted_ipca)
+    print(adusted_selic)
+
+def adjust_values_to_quarter(data):
+
+    data_quarter = pd.DataFrame(columns=data.columns)
+
+    count = 1
+    for i in range(0, len(data.index), 3):
+        grouped_months = data[i:i+3].apply(quarters_interest)
+        grouped_months.name = "%d_quarter"%count
+        data_quarter = data_quarter.append(grouped_months)
+        count+=1
+
+    return data_quarter
+
+def quarters_interest(column):
+    return (column[0] + 1) * (column[1] + 1) * (column[2] + 1) - 1
+
+main()
 
 
 
